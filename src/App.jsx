a8feae9cb181bcf0268c82f5e9340e4e54c2e6c1
Tushar Negi <negitushar81@gmail.com>
2024-11-28 17:23:56 +0530

    setting up files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './index.css'
+import Login from './components/Auth/Login'
+
+const App = () => {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
