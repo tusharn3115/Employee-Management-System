e37f790be3f3ec8b1396a4bc07e4de272ffb0953
Tushar Negi <negitushar81@gmail.com>
2024-11-30 08:11:09 +0530

    created context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import { useContext, useState } from 'react';
 import './index.css';
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AuthContext } from './context/AuthProvider';
 
 const App = () => {
-  const [user, setUser] = useState(null); // State for logged-in user
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+  
 
   const handleLogin = (email, password) => {
-    // Simulate login
     if (email === 'admin@gmail.com' && password === '123') {
-      setUser({ role: 'admin', email }); // Set admin user
+      setUser({ role: 'admin', email });
     } else if (email === 'employee@gmail.com' && password === '123') {
-      setUser({ role: 'employee', email }); // Set employee user
+      setUser({ role: 'employee', email });
     } else {
       toast.error("Invalid Credentials", {
         position: "top-right",
@@ -29,10 +31,9 @@ const App = () => {
     }
   };
 
-  // Render different dashboards based on the role
   return (
     <>
-      <ToastContainer /> {/* Add ToastContainer for notifications */}
+      <ToastContainer />
       {!user ? <Login handleLogin={handleLogin} /> : ''}
       {user && user.role === 'admin' && <AdminDashboard />}
       {user && user.role === 'employee' && <EmployeeDashboard />}
